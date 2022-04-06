2242c2f561d764aa6993a897e6620296471f7ad1
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-04-06 19:53:01 +0100

    Improvements

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 TESTS=$(ls tests/*.in)
-(test -e MyOutput && test -d MyOutput) || mkdir MyOutput
+{ test -e MyOutput && test -d MyOutput; } || mkdir MyOutput
 for TEST in $TESTS
 do
 	TESTNAME=${TEST%.in}
-	MYOUT=MyOutput/${TESTNAME#tests/}.myout
-	./a.out < $TEST > $MYOUT
-	(diff $MYOUT ${TESTNAME}.out > temp.diff && echo "${TESTNAME#tests/} passed.") || echo "${TESTNAME#tests/} failed."
+	MYOUT=MyOutput/"${TESTNAME#tests/}".myout
+	./a.out < "$TEST" > "$MYOUT"
+	{ diff "$MYOUT" "${TESTNAME}".out > temp.diff && echo "${TESTNAME#tests/ } passed."; } || echo "${TESTNAME#tests/} failed."
 done
 rm temp.diff
