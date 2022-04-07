2dbd1c0303bb64b1f81715df8d61210deb52d256
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-04-07 13:13:30 +0100

    Added flags

--- test.sh
+++ test.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
-TESTS=$(ls tests/*.in)
+
+while getopts b:t: FLAG; do
+	case $FLAG in
+		b) BIN=$OPTARG;;
+		t) TESTDIR=$OPTARG;;
+		?) echo "Invalid flag"
+	esac
+done
+
+# Default values
+[ "$BIN" = "" ] && BIN="a.out"
+[ "$TESTDIR" = "" ] && TESTDIR="tests"
+
+# Getting tests from the test directory
+TESTS=$(ls "$TESTDIR"/*.in)
+
+# Creating MyOutput directory if it doestn exist
 { test -e MyOutput && test -d MyOutput; } || mkdir MyOutput
+
+# Executing all tests
 for TEST in $TESTS
 do
 	TESTNAME=${TEST%.in}
 	MYOUT=MyOutput/"${TESTNAME#tests/}".myout
-	./a.out < "$TEST" > "$MYOUT"
-	{ diff "$MYOUT" "${TESTNAME}".out > temp.diff && echo "${TESTNAME#tests/ } passed."; } || echo "${TESTNAME#tests/} failed."
+	./"$BIN" < "$TEST" > "$MYOUT"
+	{ diff "$MYOUT" "${TESTNAME}".out > /dev/null && echo "${TESTNAME#tests/ } passed."; } || echo "${TESTNAME#tests/} failed."
 done
-rm temp.diff
