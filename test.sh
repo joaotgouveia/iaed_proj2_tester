79e436ec90263d5169f446f7443fc5164c6ed579
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-04-07 14:03:58 +0100

    Added colors

--- test.sh
+++ test.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+# test.sh - Simple testing script for projects
+
+# Defining colors
+BOLD=$(tput bold)
+RES=$(tput sgr0)
+FAILED="${BOLD}$(tput setaf 1)failed$RES" # Bold red
+PASSED="${BOLD}$(tput setaf 2)passed$RES" # Bold green
+ERROR="${BOLD}$(tput setaf 1)Erro: a flag '$1' nao existe.$RES"
 
 # Show usage
 help() {
-	[ "$1" = "" ] || printf "%s\n\n" "Erro: A flag '$1' nao existe."
-	echo "Utilizacao:"
-	echo "-b <Caminho-para-o-executavel>: Definir o executavel a testar, em caso de omicao da flag o default e' 'a.out'."
-	echo "-t <Caminho-para-a-diretoria-dos-testes>: Definir os testes, em caso de omicao da flag o default e' 'tests'."
-	echo "-h: Mostrar utilizacao."
+	[ "$1" = "" ] || printf "\t%s\n" "$ERROR"
+	printf "\t%s\n" "Utilizacao:"
+	printf "\t%s\n" "-b <Caminho-para-o-executavel>: Definir o executavel a testar, em caso de omicao da flag o default eh 'a.out'."
+	printf "\t%s\n" "-t <Caminho-para-a-diretoria-dos-testes>: Definir os testes, em caso de omicao da flag o default eh 'tests'."
+	printf "\t%s\n" "-h: Mostrar utilizacao."
+	exit 0
 }
 
 # Check for flags
-while getopts b:t:h FLAG; do
+while getopts :b:t:h FLAG; do
 	case $FLAG in
 		b) BIN="$OPTARG";;
 		t) TESTDIR="$OPTARG";;
@@ -34,5 +43,6 @@ for TEST in $TESTS; do
 	TESTNAME=${TEST%.in}
 	MYOUT=MyOutput/"${TESTNAME#tests/}".myout
 	./"$BIN" < "$TEST" > "$MYOUT"
-	{ diff "$MYOUT" "${TESTNAME}".out > /dev/null && echo "${TESTNAME#tests/ } passed."; } || echo "${TESTNAME#tests/} failed."
+	TESTNUM="${TESTNAME#tests/}"
+	{ diff "$MYOUT" "${TESTNAME}".out > /dev/null && echo "$TESTNUM $PASSED"; } || echo "$TESTNUM $FAILED"
 done
