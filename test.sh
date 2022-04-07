183be984384b14fc965facea1098519283df7772
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-04-07 13:32:20 +0100

    Added help

--- test.sh
+++ test.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-while getopts b:t: FLAG; do
+# Show usage
+help() {
+	[ "$1" = "" ] || printf "%s\n\n" "Erro: A flag '$1' nao existe."
+	echo "Utilizacao:"
+	echo "-b <Caminho-para-o-executavel>: Definir o executavel a testar, em caso de omicao da flag o default e' 'a.out'."
+	echo "-t <Caminho-para-a-diretoria-dos-testes>: Definir os testes, em caso de omicao da flag o default e' 'tests'."
+	echo "-h: Mostrar utilizacao."
+}
+
+# Check for flags
+while getopts b:t:h FLAG; do
 	case $FLAG in
-		b) BIN=$OPTARG;;
-		t) TESTDIR=$OPTARG;;
-		?) echo "Invalid flag"
+		b) BIN="$OPTARG";;
+		t) TESTDIR="$OPTARG";;
+		h) help;;
+		?) help "$OPTARG";;
 	esac
 done
 
-# Default values
+# Default values if a flag is not provided
 [ "$BIN" = "" ] && BIN="a.out"
 [ "$TESTDIR" = "" ] && TESTDIR="tests"
 
@@ -19,8 +30,7 @@ TESTS=$(ls "$TESTDIR"/*.in)
 { test -e MyOutput && test -d MyOutput; } || mkdir MyOutput
 
 # Executing all tests
-for TEST in $TESTS
-do
+for TEST in $TESTS; do
 	TESTNAME=${TEST%.in}
 	MYOUT=MyOutput/"${TESTNAME#tests/}".myout
 	./"$BIN" < "$TEST" > "$MYOUT"
